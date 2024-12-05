8b45366a90941a29e2c5acaa03e048c67ee0bccc
Elio GENCE <83227001+ElioGence@users.noreply.github.com>
2024-12-06 00:57:15 +0100

    Add files via upload

--- KNN/KNNTraining.py
+++ KNN/KNNTraining.py
@@ -2,10 +2,19 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import validation_curve
+from sklearn.model_selection import learning_curve
 import pandas as pd
 import matplotlib.pyplot as plt
 from ModelSetup import *
 from FileManipulation import *
+
+"""
+Need to update to avoid overfitting here some possible measures :
+Distance Weights
+Distance Metric
+Reduce Noise in Data 
+More Data (some categories are under represented)
+"""
 
 def get_Best_Number_Neighbors(X,y,kmax=30):
     """
@@ -46,13 +55,10 @@ def get_Best_Number_Neighbors(X,y,kmax=30):
     
     return best_k, best_score
 
-def save_Scores(model, X_train, X_test, y_train, y_test, kmax=20):
+def save_Validation_Scores(model, X_train, y_train, kmax=20):
     """
-    Function to save the scores for train and test (only use this if you want to save the validation curve)
-    
-    ! NEED TO COMPARE MORE THAN JUST KNN, LIKE TRAIN SIZE FOR EXAMPLE !
+    Function to save the scores for train and test (only use this if you want to save the validation curve otherwise redundant)
     """
-    
     k = np.arange(1, kmax)
 
     train_scores, val_scores = validation_curve(model, X_train, y_train,
@@ -66,9 +72,23 @@ def save_Scores(model, X_train, X_test, y_train, y_test, kmax=20):
     plt.title("Validation Curve")
     plt.legend()
     plt.grid()
-    save_Plot("Scores.png")
+    save_Plot("KNN_Validation_Scores.png")
     plt.show()
 
+def save_Learning_Curve(model, X_train, y_train):
+    """
+    Function to save the learning curve
+    """
+    N, train_score, val_score = learning_curve(model, X_train, y_train,
+                                           train_sizes=np.linspace(0.1, 1, 10), cv=5)
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(N, train_score.mean(axis=1), label='train')
+    plt.plot(N, val_score.mean(axis=1), label='validation')
+    plt.xlabel('train_sizes')
+    plt.legend()
+    save_Plot("KNN_Learning_Curve.png")
+    plt.show()
 
 Data = load_All_Data("datasets/training")
 
@@ -78,11 +98,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 best_k, best_score=get_Best_Number_Neighbors(X_train,y_train,30)
 
 model = KNeighborsClassifier(n_neighbors=best_k)
+
+#save_Validation_Scores(X_train, y_train)
+
+save_Learning_Curve(model, X_train, y_train)
+
 model.fit(X_train, y_train)
-
-save_Scores(model, X_train, X_test, y_train, y_test)
-
-predictedDF=setup_Prediction(model,X_train)
+predictedDF=setup_Prediction(model,X_train) 
+""" UPDATE TO MAKE A PREDICTION ON THE TEST VALUES"""
 
 save_Prediction(predictedDF, "Result_KNN.csv", Data)
 """ Last line need to be changed so that the differences can be visualised instead of just saving a csv"""
